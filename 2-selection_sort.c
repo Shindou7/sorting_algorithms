fc83e4c9554466125eaaa1017873f5c880eea0d9
Shindou7 <sh.shindou@gmail.com>
2023-09-25 12:12:10 -0700

    s

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,28 +7,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, sort;
-    int temp;
+	size_t i, j, sort;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        sort = i;
-
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[sort])
-                sort = j;
-        }
-
-        if (sort != i)
-        {
-            temp = array[i];
-            array[i] = array[sort];
-            array[sort] = temp;
-            print_array(array, size);
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		sort = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[sort])
+				sort = j;
+		}
+		if (sort != i)
+		{
+			temp = array[i];
+			array[i] = array[sort];
+			array[sort] = temp;
+			print_array(array, size);
+		}
+	}
 }
